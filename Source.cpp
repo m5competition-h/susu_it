a7280a6b05e8c9f99a4ba7e9a39632ec833a62ff
Ivan Nikulin <ivan.nikulin.2005@inbox.ru>
2024-12-17 13:58:01 +0500

    Изменил текст сообщения при выводе расстояния на следующий "Расстояние между двумя точками (x0, y0) и (x1, y1): ". Вместо x0, y0, x1, y1, подставляются введенные пользователем координаты

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-double distance(double a_x, double a_y, double b_x, double b_y) {
-	double dx = a_x - b_x, dy = a_y - b_y;
+int distance(int a_x, int a_y, int b_x, int b_y) {
+	int dx = a_x - b_x, dy = a_y - b_y;
 	return sqrt(dx * dx + dy * dy);
 }
 
 int main() {
 	system("chcp 1251");
-	double a_x, a_y;
+	int a_x, a_y;
 	cout << "a_x, a_y: ";
 	cin >> a_x >> a_y;
-	double b_x, b_y;
+	int b_x, b_y;
 	cout << "b_x, b_y: ";
 	cin >> b_x >> b_y;
-	cout << "дистанция между точками : " << distance(a_x, a_y, b_x, b_y) << endl;
+	cout << "Расстояние между двумя точками:" << "(" << a_x << "," << " " << a_y << ")" << "(" << b_x << "," << " " << b_y << "):" << distance(a_x, a_y, b_x, b_y) << endl;
 }
